c070e871e013a9bda00e96977756adc525efc3c9
mhtess <mht137@gmail.com>
2014-11-13 11:33:36 -0800

    Rewrite premise-prior to handle unequal no. of alternatives

--- models/syll_chwrite.py
+++ models/syll_chwrite.py
@@ -8,7 +8,8 @@ def write_church(pd):
     print str(pd['n_balls']) + ' = n balls'
 
     # to make church model more readable
-    sylldict = {"pm":"A-B","mp":"B-A","ms":"B-C","sm":"C-B","ps":"A-C","sp":"C-A","A":"all.","E":"none.","I":"some.","O":"not-all.","P":"A","M":"B","S":"C"}
+    sylldict = {"pm":"A-B","mp":"B-A","ms":"B-C","sm":"C-B","ps":"A-C","sp":"C-A","A":"all.",\
+                "E":"none.","I":"some.","O":"not-all.","N":"there-is-no.","P":"A","M":"B","S":"C"}
 
     fid = open(pd['fname'],'w')
     # <rawcell>
@@ -23,13 +24,27 @@ def write_church(pd):
     # write down premise prior
     fid.write('(define premise-prior (lambda (figure)\n')
     fid.write('\t(case figure\n')
-    lp = 16
-    figs = divmod(len(pd['posspremises']),lp)[0]
-    for h in range(1,figs+1):
+    #lp = 16
+    if pd['fig'] == 'Full':
+        figs = 4
+    else:
+        figs = 1
+
+    h = 0
+    count = 0
+    while h<figs:
+        print count
         fid.write('\t\t((%d) (uniform-draw (list ' % h)
-        for e in range(lp):
-            fid.write("(list '%s '%s) " % ((sylldict[pd['posspremises'][e+(lp*(h-1))][2]]+sylldict[pd['posspremises'][e+(lp*(h-1))][1]]),(sylldict[pd['posspremises'][e+(lp*(h-1))][3]]+sylldict[pd['posspremises'][e+(lp*(h-1))][0]])))
+        anchor = pd['posspremises'][count][0:2]
+        while ((pd['posspremises'][count][0:2] == anchor) & (count<(len(pd['posspremises'])-1))):
+            fid.write("(list '%s '%s) " % \
+                ((sylldict[pd['posspremises'][count][3]]+sylldict[pd['posspremises'][count][1]]),\
+                 (sylldict[pd['posspremises'][count][2]]+sylldict[pd['posspremises'][count][0]])))
+            count = count + 1
+
+        h = h + 1
         fid.write(')))\n')
+
     fid.write(')))\n\n')
 
     # write down situation prior
@@ -123,7 +138,7 @@ def write_church(pd):
     # <codecell>
     fid.write('(define allprems (list ')
     for p in pd['premises']:
-        fid.write('%s%s%s %s%s%s%s\t' % ("(list '",sylldict[p[2]],sylldict[p[1]],"'",sylldict[p[3]],sylldict[p[0]],")"))
+        fid.write('%s%s%s %s%s%s%s\t' % ("(list '",sylldict[p[3]],sylldict[p[1]],"'",sylldict[p[2]],sylldict[p[0]],")"))
     fid.write('))\n\n')
 
     # for z in range(int(pd['ndepth'])+1):
@@ -137,7 +152,7 @@ def write_church(pd):
     # fid.write('\n')
     fid.write('(define figures (list')
     for h in range(1,figs+1):
-        for e in range(lp):
+        for e in range(16):
             fid.write(' %d' % h)
     fid.write('))\n\n')
     fid.write('(define args\n\t(map string->number (regexp_split argstring ",")))\n\n')
